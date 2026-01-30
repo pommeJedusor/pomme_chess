e716ae9fab304c184fa32b7e30752fa1b5744981
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2026-01-30 19:30:29 +0100

    updated get_moves to actually return the moves

--- src/main.rs
+++ src/main.rs
@@ -165,42 +165,38 @@ impl ChessBoard {
         fen_board + " " + &fen_player_turn + " " + &fen_castles
     }
 
-    fn get_rook_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_rook_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         let square_mask = &ma.rook_moves_masks_magical_numbers[index as usize].mask;
         let magical_number = &ma.rook_moves_masks_magical_numbers[index as usize].magic_number;
         let board = self.player | self.opponent;
         let hashkey = (board & square_mask).wrapping_mul(*magical_number) >> 48;
         let rook_moves = ma.rook_mask_blockers_hashmaps[index as usize][hashkey as usize].unwrap();
-        //println!("rook");
-        //binary_mask::print_mask(rook_moves & self.player ^ rook_moves);
-        rook_moves & self.player ^ rook_moves
+        move_mask_to_u16(index, rook_moves & self.player ^ rook_moves)
     }
 
-    fn get_bishop_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_bishop_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         let square_mask = &ma.bishop_moves_masks_magical_numbers[index as usize].mask;
         let magical_number = &ma.bishop_moves_masks_magical_numbers[index as usize].magic_number;
         let board = self.player | self.opponent;
         let hashkey = (board & square_mask).wrapping_mul(*magical_number) >> 48;
         let bishop_moves =
             ma.bishop_mask_blockers_hashmaps[index as usize][hashkey as usize].unwrap();
-        //println!("bishop");
-        //binary_mask::print_mask(bishop_moves & self.player ^ bishop_moves);
-        bishop_moves & self.player ^ bishop_moves
+        move_mask_to_u16(index, bishop_moves & self.player ^ bishop_moves)
     }
 
-    fn get_knight_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_knight_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         let knight_moves = ma.knight_move_masks[index as usize];
-        println!("knight");
-        binary_mask::print_mask(knight_moves & self.player ^ knight_moves);
-        knight_moves & self.player ^ knight_moves
+        move_mask_to_u16(index, knight_moves & self.player ^ knight_moves)
     }
 
-    fn get_queen_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_queen_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         // TODO optimize specifically for the queen
-        self.get_bishop_moves(index, ma) | self.get_rook_moves(index, ma)
+        let mut queen_moves = self.get_bishop_moves(index, ma);
+        queen_moves.append(&mut self.get_rook_moves(index, ma));
+        queen_moves
     }
 
-    fn get_pawn_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_pawn_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         let index = index as usize;
         let color = !self.is_white_to_play as usize;
         let pawn_takes =
@@ -211,39 +207,38 @@ impl ChessBoard {
             | (pawn_blockers >> ma.pawn_offsets[color][index][0]) & 0b11;
         let pawn_moves = ma.pawn_mask_blockers_hashmaps[color][index][hashkey as usize];
         let moves = pawn_moves | pawn_takes;
-        println!("pawn");
-        binary_mask::print_mask(moves);
-        moves
+        move_mask_to_u16(index as u8, moves)
     }
 
-    fn get_king_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> u64 {
+    fn get_king_moves(&self, index: u8, ma: &binary_mask::MainHashtables) -> Vec<u16> {
         // TODO add castling
         let king_moves = ma.king_move_masks[index as usize];
-        println!("king");
-        binary_mask::print_mask(king_moves & self.player ^ king_moves);
-        king_moves & self.player ^ king_moves
+        move_mask_to_u16(index as u8, king_moves & self.player ^ king_moves)
     }
 
-    fn get_moves(&self, ma: binary_mask::MainHashtables) {
+    fn get_moves(&self, ma: binary_mask::MainHashtables) -> Vec<u16> {
+        // TODO use something else than a vec
+        let mut moves: Vec<u16> = vec![];
         let pieces = &self.player_pieces;
 
         for i in 0..64 {
             // TODO continue; if square unoccupied by current player
             let index = 1 << i;
             if index & pieces.pawns != 0 {
-                self.get_pawn_moves(i, &ma);
+                moves.append(&mut self.get_pawn_moves(i, &ma));
             } else if index & pieces.king != 0 {
-                self.get_king_moves(i, &ma);
+                moves.append(&mut self.get_king_moves(i, &ma));
             } else if index & pieces.rooks != 0 {
-                self.get_rook_moves(i, &ma);
+                moves.append(&mut self.get_rook_moves(i, &ma));
             } else if index & pieces.bishops != 0 {
-                self.get_bishop_moves(i, &ma);
+                moves.append(&mut self.get_bishop_moves(i, &ma));
             } else if index & pieces.knights != 0 {
-                self.get_knight_moves(i, &ma);
+                moves.append(&mut self.get_knight_moves(i, &ma));
             } else if index & pieces.queens != 0 {
-                self.get_queen_moves(i, &ma);
+                moves.append(&mut self.get_queen_moves(i, &ma));
             }
         }
+        moves
     }
 }
 
@@ -251,5 +246,6 @@ fn main() {
     let ma = binary_mask::generate_main_hashtables();
     let chessboard = get_starting_chessboard();
     print_mask(chessboard.player | chessboard.opponent);
-    chessboard.get_moves(ma);
+    let moves = chessboard.get_moves(ma);
+    println!("{:?}", moves);
 }
